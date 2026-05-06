3a55f6aaf6d2b1f0be40f23c98e55c071a04b31f
Mohammad <lmohammad231l@gmail.com>
2026-05-06 14:22:17 +0300

    fix structer for Home page

--- project/client/Smart-pharmacy-frontend/src/App.jsx
+++ project/client/Smart-pharmacy-frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
-import Navbar from './/component/Navbar '
+
 
 function App() {
   return (
     <div className="min-h-screen bg-white">
-      <Navbar />
+      
       <Routes>
         <Route path="/" element={<Home />} />
 
--- project/client/Smart-pharmacy-frontend/src/pages/Home.jsx
+++ project/client/Smart-pharmacy-frontend/src/pages/Home.jsx
@@ -1,13 +1,15 @@
-import Hero     from '../component/Hero'
-import About    from '../component/About'
-import Features from '../component/Features'
-import Team     from '../component/Team'
-import CTA      from '../component/Cta'
-import Footer   from '../component/Footer '
+import Hero     from '../component/CompHome/Hero'
+import About    from '../component/CompHome/About'
+import Features from '../component/CompHome/Features'
+import Team     from '../component/CompHome/Team'
+import CTA      from '../component/CompHome/Cta'
+import Footer   from '../component/Footer'
+import Navbar   from '../component/Navbar'
 
 export default function Home() {
   return (
     <>
+      <Navbar />
       <Hero />
       <About />
       <Features />
